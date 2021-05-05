526e5d14c1124cef4aa3ceecbf4cd71e03b91a64
Merjema Sarajlija <merjema.s@hotmail.com>
2021-05-05 21:08:21 +0200

    media embed

--- src/video.js
+++ src/video.js
@@ -2,10 +2,12 @@ import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
 import Widget from '@ckeditor/ckeditor5-widget/src/widget';
 import VideoEditing from "./video/videoediting";
 import '../theme/video.css';
+import AutoMediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembedui';
+
 
 export default class Video extends Plugin {
     static get requires() {
-        return [ VideoEditing, Widget];
+        return [ VideoEditing, Widget, AutoMediaEmbed];
     }
 
     static get pluginName() {
